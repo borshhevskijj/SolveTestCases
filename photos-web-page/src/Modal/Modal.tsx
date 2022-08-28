6f9afbd2bd5d6b583c3918c1886f2f97718455b6
borshhevskijj <borshhevskijj98@mail.ru>
2022-08-28 18:41:44 +0300

    added Photos & Modal components

---
+++ photos-web-page/src/Modal/Modal.tsx
@@ -0,0 +1,35 @@
+import React, { useId } from 'react'
+// import { Iphotos } from './../photos/Photos';
+import cl from './modal.module.css'
+
+interface IModalWindow {
+  isActive: boolean
+  setIsActive: (value: boolean) => void
+  children: JSX.Element | JSX.Element[]
+  // photo: Iphotos
+}
+
+
+
+export const Modal = ({ isActive, setIsActive, children }: IModalWindow) => {
+  const uId = useId()
+  // console.log(uId);
+
+
+  return (
+    <>
+
+      <section key={uId} className={isActive ? cl.modalContainer + ' ' + cl.active : cl.modalContainer} onClick={() => setIsActive(false)}>
+        <div key={uId} onClick={(e) => e.stopPropagation} className={isActive ? cl.modalContent + ' ' + cl.active : cl.modalContent}>
+          {children}
+        </div>
+      </section>
+
+    </>
+
+
+
+
+
+  )
+}
--- photos-web-page/src/photos/Photos.tsx
+++ photos-web-page/src/photos/Photos.tsx
@@ -1,39 +1,74 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { Modal } from '../Modal/Modal';
+import { Loader } from './../Loader';
+import cl from './photos.module.css'
 
 
-interface Iphotos {
+export interface Iphotos {
   id: number
   url: string
+  comments?: string[]
 }
+
 export const Photos = () => {
+  const [isActive, setIsActive] = useState(false)
+  const [currentLargePhoto, setCurrentLargePhoto] = useState<Iphotos | undefined>(undefined)
+  const [photos, setPhotos] = useState<Iphotos[] | undefined>()
+  const [isLoaded, setIsLoaded] = useState(false)
+
+
+
+  const getPhotos = async () => {
+    const response = await axios.get('https://boiling-refuge-66454.herokuapp.com/images')
+      .then(res => res.data)
+    setPhotos(response)
+  }
 
   useEffect(() => {
     try {
-      axios.get('https://boiling-refuge-66454.herokuapp.com/images')
-        .then(res => {
-          const respons = res.data
-          setPhotos(respons)
-        })
-
-    } catch (error) {
+      getPhotos()
+    }
+    catch (error) {
       alert(`ошибка - ${error}`)
     }
 
   }, [])
 
-  const [photos, setPhotos] = useState<Iphotos[] | undefined>()
 
+
+  const openModal = async (e: any, id: number) => {
+    try {
+      const largePhoto = await axios.get(`https://boiling-refuge-66454.herokuapp.com/images/${id}`)
+        .then((res: any) => res.data)
+      setCurrentLargePhoto(largePhoto)
+      setIsActive(true)
+      setIsLoaded(true)
+    }
+    catch (error) {
+      alert(`ошибка - ${error}`)
+    }
+
+  }
 
   return (
-    <>
+    <section className={cl.photoContainer}>
       {
-        photos ?
-          photos.map((photo: Iphotos) =>
-            <img src={photo.url} key={photo.id} alt="" />
-          )
-          : <p>ничего не найдено</p>
+        photos
+          ? photos.map((photo: Iphotos) =>
+            <img src={photo.url} key={photo.id} onClick={(e: any) => openModal(e, photo.id)} alt="" />)
+          : <Loader />
       }
-    </>
+      <Modal isActive={isActive} setIsActive={setIsActive} children={
+        <>
+          <img src={currentLargePhoto?.url} alt="" />
+          {
+            !currentLargePhoto?.comments
+            ?? currentLargePhoto.comments?.map((photo: string) => <p>{photo} </p>)
+          }
+        </>
+      } />
+
+    </section>
   )
 }
